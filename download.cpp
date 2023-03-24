0317cdbdad990570c89fae075feb85f2ee2dd90a
Albert <103274848+Albert311a@users.noreply.github.com>
2023-03-24 11:42:15 -0400

    Add files via upload

---
+++ download.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+class progress_bar{
+
+        std::vector<std::string> vs;
+
+    public:
+
+        int screen_width( ){
+            struct winsize w;
+            ioctl( STDOUT_FILENO, TIOCGWINSZ, &w );
+            int i = w.ws_col;
+            return i;
+        }
+
+        void read_file( ){
+            std::ifstream ifile;
+            ifile.open( "list2" );
+            std::string line;
+            while( getline( ifile, line ) ){
+                line += "-" + get_ver( );
+                vs.push_back( line );
+            }
+        }
+
+        std::string get_ver( ){
+            std::default_random_engine gen( ( std::random_device( ) ) ( ) );
+            std::uniform_int_distribution<int> dig( 0, 9 );
+            std::uniform_int_distribution<int> sz( 2, 4 );
+            int n = sz( gen );
+
+            std::string numb = std::to_string( dig( gen ) );
+            for( int i = 0; i < n; i++ ){
+                numb += "." + std::to_string( dig( gen ) );
+            }
+            return numb;
+        }
+
+        void print_bar( const std::string str, const int perc, int sz ){
+            std::cout << "\r\033[K";
+            std::string progress = " [" + std::string( perc, '*' ) + std::string( sz - perc, ' ' ) + "]";
+            std::cout << str << "\t"<< progress << "\033[F";
+        }
+
+        void bar( const std::string &str, const int sz ){
+            for( int i = 0; i <= sz; i++ ){
+                std::cout << std::endl;
+                print_bar( str, i, sz );
+                usleep( 10000 );
+            }
+        }
+
+        void start( ){
+            int k = screen_width( );
+            for( int i = 0; i < 10; i++ ){
+                std::string v = vs[ i ];
+                bar( v, k * .5 );
+                std::cout << "\033[F";
+                std::cout << std::endl << std::endl;
+                std::cout << v << "\033[K\033[F" << std::endl;
+            }
+        }
+};
+
+int main( int argc, char *argv[ ] ){
+    progress_bar p;
+    p.read_file( );
+    std::cout << "\033\e[?25l";
+    p.start( );
+    std::cout << "\033\e[?25h";
+    std::cout << std::endl << std::endl;
+
+	return 0;
+}
+
